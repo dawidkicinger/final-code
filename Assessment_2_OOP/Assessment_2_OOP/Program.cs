106a0df91b57c8d5a49e4dc753dc39891c1266f7
Dawid Piotr Kicinger <19701329@students.lincoln.ac.uk>
2020-03-26 08:50:00 +0000

    got rid of quiz

--- Assessment_2_OOP/Assessment_2_OOP/Program.cs
+++ Assessment_2_OOP/Assessment_2_OOP/Program.cs
@@ -7,341 +7,43 @@ namespace Assessment_2_OOP
     {   
         //a couple of variables that I am going to use throughout the program, and I have added one variable to make NewLine(to simplify command and to reduce amount of repetition of long command)
         public string NewLine = Environment.NewLine;
-        public string option;
-        public string Name;
-        public int score;
         public string answer;
         
         static void Main(string[] args)
         {
             //local variable made and some lines of code asking what the user wants to do
             Program p = new Program();
-            Console.WriteLine("If you would like to start a quiz press 1");
-            Console.WriteLine("If you would like to start a file comparison press 2");
-            string option = Console.ReadLine();
-            //if 1 is pressed will go to the questionaire if 2 pressed will check files
-            if (option == "1")
-            {
-                p.Questions();
-            }
-            else if (option == "2")
-            {
-                p.CheckFiles();
-            }
-            else
-            {
-                Console.WriteLine("That input Is Not Valid, Try Again. ");
-            }
+            Console.WriteLine("Welcome to File checker");
+            p.CheckFiles();
             
         }
-        public void Questions()
-        {
-            score = 0;
-        start:
-            //a couple of lines of code that will print a couple of messages on the screen
-            Console.WriteLine("Welcome to ASQ - A Short Questionaire");
-            Console.WriteLine("This questionaire is made up of 10 questions");
-            Console.WriteLine("Questionaire Made By: Dawid Kicinger");
-            Console.WriteLine(NewLine + "Options" + NewLine + "Make sure to press enter after you have chosen the number (every time)");
-            Console.WriteLine("If you would like to start the program       press 1");
-            Console.WriteLine("If you would like to see instructions        press 2");
-            Console.WriteLine("If you would like to quit                    press 3");
-            option = Console.ReadLine();
-        input:
-            
-            //if 1 pressed program will start, if 2 pressed instuctions will be shown if 3 pressed the program will quit
-            if (option == "1")
-            {
-                Console.WriteLine("Before you start");
-                Console.WriteLine("What is your name?");
-                Name = Console.ReadLine();
-                Question1();
-            }
 
-            else if (option == "2")
+        public void CheckFiles()
+        //this is checking for files, comparing them and then letting the user know if they are the same or different
+        {
+            Console.WriteLine("Would you like to start the program?" + NewLine);
+            Console.WriteLine("Options");
+            Console.WriteLine("Yes");
+            Console.WriteLine("No");
+            answer = Console.ReadLine();
+            if (answer == "Yes")
             {
-                Console.WriteLine("To answer each question just write down a number corresponding to a specific answer");
-                Console.WriteLine("BE CAREFUL! 1 Question wrong and you have to start from the beggining.");
-                Console.WriteLine("This is a multiple choice questionaire.");
-                Console.WriteLine("Make sure you have fun." + NewLine);
-                goto start;
+                CheckingFiles();
             }
-            else if (option == "3")
+            else if (answer == "yes")
+            {
+                CheckingFiles();
+            }
+            else if (answer == "No")
             {
                 return;
             }
-            else
+            else if (answer == "no")
             {
-                Console.WriteLine("That input Is Not Valid, Try Again. ");
-                goto input;
+                return;
             }
         }
-        public void Question1()
-            //This code repeats the same 10 times, it will print some lines of code letting the user know the question and options availabe, it will then ask for an answer and will then check if answer is the same as it should be if it is it will go to next question otherwise, it will send you to the Questions() function
-        {
-            
-            Console.WriteLine("What is 19+21?");
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("39");
-            Console.WriteLine("40");
-            Console.WriteLine("50");
-            Console.WriteLine("49" + NewLine);
-            Console.WriteLine("Type your answer here = ");
-            answer = Console.ReadLine();
-            if (answer == "40")
-            {
-                //for each of the answer correct it will add 1 point on to the score
-                score = +1;
-                Question2();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Console.WriteLine("Good Job, " + Name);
-                Questions();
-            }
-            
-        }
-        public void Question2()
-        {
-            Console.WriteLine("What is pi to 4 decimal places?" + NewLine);
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("3.1415");
-            Console.WriteLine("3.1514");
-            Console.WriteLine("3.1417");
-            Console.WriteLine("3.1517" + NewLine);
-            Console.WriteLine("Type your answer here = ");
-            answer = Console.ReadLine();
-            if (answer == "3.1415")
-            {
-                score = +1;
-                Question3();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Console.WriteLine("Good Job, " + Name);
-                Questions();
-            }
-
-        }
-        public void Question3()
-        {
-            Console.WriteLine("In what city in England can you find home of George Boole?" + NewLine);
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("Boston");
-            Console.WriteLine("Manchester");
-            Console.WriteLine("London");
-            Console.WriteLine("Lincoln" + NewLine);
-            Console.WriteLine("Type your answer here = ");
-            answer = Console.ReadLine();
-            if (answer == "Lincoln")
-            {
-                score = +1;
-                Question4();
-            }
-            else if (answer == "lincoln")
-            {
-                score = +1;
-                Question4();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Questions();
-            }
-        }
-        public void Question4()
-        {
-            Console.WriteLine("In what year did Princess Diana die?" + NewLine);
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("1996");
-            Console.WriteLine("1997");
-            Console.WriteLine("1998");
-            Console.WriteLine("1999" + NewLine);
-            Console.WriteLine("Type your answer here = ");
-            answer = Console.ReadLine();
-            if (answer == "1997")
-            {
-                score = +1;
-                Question5();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Console.WriteLine("Good Job, " + Name);
-                Questions();
-            }
-        }
-        public void Question5()
-        {
-            Console.WriteLine("What is the most fractured human bone?" + NewLine);
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("Brain");
-            Console.WriteLine("Clavicle");
-            Console.WriteLine("Finger");
-            Console.WriteLine("Mandible" + NewLine);
-            Console.WriteLine("Type your answer here = ");
-            answer = Console.ReadLine();
-            if (answer == "Clavicle")
-            {
-                score = +1;
-                Question6();
-            }
-            else if (answer == "clavicle")
-            {
-                score = +1;
-                Question6();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Console.WriteLine("Good Job, " + Name);
-                Questions();
-            }
-        }
-        public void Question6()
-        {
-            Console.WriteLine("What is the National Symbol of Poland?" + NewLine);
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("White and Red");
-            Console.WriteLine("Hawk");
-            Console.WriteLine("Bear");
-            Console.WriteLine("Eagle" + NewLine);
-            Console.WriteLine("Type your answer here = ");
-
-            answer = Console.ReadLine();
-            if (answer == "Eagle")
-            {
-                score = +1;
-                Question7();
-            }
-            else if (answer == "eagle")
-            {
-                score = +1;
-                Question7();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Console.WriteLine("Good Job, " + Name);
-                Questions();
-            }
-        }
-        public void Question7()
-        {
-            Console.WriteLine("What is Japanese sake made from" + NewLine);
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("Pasta");
-            Console.WriteLine("Noodles");
-            Console.WriteLine("Rice");
-            Console.WriteLine("Salad" + NewLine);
-            Console.WriteLine("Type your answer here = ");
-            answer = Console.ReadLine();
-            if (answer == "Rice")
-            {
-                score = +1;
-                Question8();
-            }
-            else if (answer == "rice")
-            {
-                score = +1;
-                Question8();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Console.WriteLine("Good Job, " + Name);
-                Questions();
-            }
-        }
-        public void Question8()
-        {
-            Console.WriteLine("What is the real meaning of the Greek word 'Pita'" + NewLine);
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("Cake");
-            Console.WriteLine("Bread");
-            Console.WriteLine("Wheat");
-            Console.WriteLine("Kebab" + NewLine);
-            Console.WriteLine("Type your answer here = ");
-            answer = Console.ReadLine();
-            if (answer == "Bread")
-            {
-                score = +1;
-                Question9();
-            }
-            else if (answer == "bread")
-            {
-                score = +1;
-                Question9();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Console.WriteLine("Good Job, " + Name);
-                Questions();
-            }
-        }
-        public void Question9()
-        {
-            Console.WriteLine("What country does the Peroni beer originate from?" + NewLine);
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("Italy");
-            Console.WriteLine("Poland");
-            Console.WriteLine("England");
-            Console.WriteLine("Bulgaria" + NewLine);
-            Console.WriteLine("Type your answer here = ");
-            answer = Console.ReadLine();
-            if (answer == "Italy")
-            {
-                score = +1;
-                Question10();
-            }
-            else if (answer == "italy")
-            {
-                score = +1;
-                Question10();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Console.WriteLine("Good Job, " + Name);
-                Questions();
-            }
-        }
-        public void Question10()
-        {
-            Console.WriteLine("What is First Name of the creator of this quiz?" + NewLine);
-            Console.WriteLine("Options" + NewLine);
-            Console.WriteLine("Dawid");
-            Console.WriteLine("David");
-            Console.WriteLine("Kicinger");
-            Console.WriteLine("Kissinger");
-            answer = Console.ReadLine();
-            if (answer == "Dawid")
-            {
-                score = +1;
-                Congratulations();
-            }
-            else if (answer == "dawid")
-            {
-                score = +1;
-                Congratulations();
-            }
-            else
-            {
-                Console.WriteLine("Answer incorrect your final score is " + score);
-                Console.WriteLine("Good Job, " + Name);
-                Questions();
-            }
-        }
-        public void Congratulations()
-        {
-            Console.WriteLine("Congratulations " + Name + " , you have completed the quiz with " + score + "points");
-            Questions();
-        }
-        public void CheckFiles()
-            //this is checking for files, comparing them and then letting the user know if they are the same or different
+        public void CheckingFiles()
         {
             string file1 = File.ReadAllText(@"GitRepositories_1a.txt"); 
             string file2 = File.ReadAllText(@"GitRepositories_1b.txt");
@@ -373,7 +75,7 @@ namespace Assessment_2_OOP
             {
                 Console.WriteLine("GitRepositories_3a and GitRepositories_3b are different"  + NewLine);
             }
-            Questions();
+            CheckFiles();
         }
         
     }
